3920b94e244f279e8ff6384806ef15a3e4855099
kakou34 <kaouther.mouheb@gmail.com>
2022-12-24 20:24:53 +0100

    Update gen_physical_lms.sh

--- antspy/gen_physical_lms.sh
+++ antspy/gen_physical_lms.sh
@@ -9,21 +9,3 @@ ImageMath 3 /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd
 
 ImageMath 3 /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd4/copd4_eBHCT_lm.csv LabelStats /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd4/copd4_eBHCT_lm.nii.gz
 ImageMath 3 /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd4/copd4_iBHCT_lm.csv LabelStats /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd4/copd4_iBHCT_lm.nii.gz
-
-ImageMath 3 /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd5/copd5_eBHCT_lm.csv LabelStats /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd5/copd5_eBHCT_lm.nii.gz
-ImageMath 3 /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd5/copd5_iBHCT_lm.csv LabelStats /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd5/copd5_iBHCT_lm.nii.gz
-
-ImageMath 3 /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd6/copd6_eBHCT_lm.csv LabelStats /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd6/copd6_eBHCT_lm.nii.gz
-ImageMath 3 /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd6/copd6_iBHCT_lm.csv LabelStats /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd6/copd6_iBHCT_lm.nii.gz
-
-ImageMath 3 /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd7/copd7_eBHCT_lm.csv LabelStats /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd7/copd7_eBHCT_lm.nii.gz
-ImageMath 3 /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd7/copd7_iBHCT_lm.csv LabelStats /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd7/copd7_iBHCT_lm.nii.gz
-
-ImageMath 3 /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd8/copd8_eBHCT_lm.csv LabelStats /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd8/copd8_eBHCT_lm.nii.gz
-ImageMath 3 /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd8/copd8_iBHCT_lm.csv LabelStats /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd8/copd8_iBHCT_lm.nii.gz
-
-ImageMath 3 /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd9/copd9_eBHCT_lm.csv LabelStats /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd9/copd9_eBHCT_lm.nii.gz
-ImageMath 3 /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd9/copd9_iBHCT_lm.csv LabelStats /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd9/copd9_iBHCT_lm.nii.gz
-
-ImageMath 3 /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd10/copd10_eBHCT_lm.csv LabelStats /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd10/copd10_eBHCT_lm.nii.gz
-ImageMath 3 /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd10/copd10_iBHCT_lm.csv LabelStats /home/kaouther/Documents/GitHub/mira_final_project/data/dir_lab_copd/copd10/copd10_iBHCT_lm.nii.gz
